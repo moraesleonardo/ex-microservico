42e8d266125f084aa3c77642436d5100aa34adb0
Leonardo <leonardobastosmoraes@hotmail.com>
2024-08-03 08:54:51 -0300

    Initial commit or your message

---
+++ src/main/java/info/moraes/ex_microservico/ExMicroservicoApplication.java
@@ -0,0 +1,13 @@
+package info.moraes.ex_microservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMicroservicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExMicroservicoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/moraes/ex_microservico/ExMicroservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package info.moraes.ex_microservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExMicroservicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
